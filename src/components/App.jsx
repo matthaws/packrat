03c7ae5f969a13f17598909e36a7d8055aaf4fb8
Matt Haws <matthaws@gmail.com>
2018-01-22 09:05:00 -0500

    Add welcome component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
-import Header from './components/header/Header.jsx'
+import { Switch, Route } from "react-router-dom";
+import Header from './header/Header.jsx'
+import Welcome from "./welcome/Welcome.jsx";
 import './App.css';
 
 const App = () => {
   return (
       <div className="App">
         <Header />
+        <Switch>
+          <Route path="/" component={Welcome} />
+        </Switch>
       </div>
     );
 }
---
+++ src/components/welcome/Welcome.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import mouse from "../../../public/mouse-512.png"
+
+const Welcome = () => (
+  <main id="welcome">
+    <img id="main-logo" src={mouse} />
+  </main>
+);
+
+export default Welcome;

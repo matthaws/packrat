4f7d0482852691f8a14f367a87ef6e570776d9fe
Ommi Shimizu <oshimizu15@gmail.com>
2018-02-14 21:46:21 -0500

    Import and render article filter component

--- src/components/welcome/Welcome.jsx
+++ src/components/welcome/Welcome.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import mouse from "./mouse-512.png";
+import ArticleFilter from "../articles/articleFilterContainer.js";
 import ArticleIndex from "../articles/articleIndexContainer.js";
 import "./welcome.css";
 
 const Welcome = () => (
   <main id="welcome">
     <img id="main-logo" src={mouse} alt="packrat logo"/>
+    <ArticleFilter/>
     <ArticleIndex/>
   </main>
 );

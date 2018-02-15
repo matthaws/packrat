5af68b87b7012921ad01d4a5a7a10d2a2ba27998
Ommi Shimizu <oshimizu15@gmail.com>
2018-02-14 21:47:40 -0500

    Add UI reducer to root reducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import articles from "./articlesReducer";
 import categories from "./categoriesReducer";
+import ui from "./uiReducer";
 
 export default combineReducers({
   articles,
   categories,
+  ui,
 });

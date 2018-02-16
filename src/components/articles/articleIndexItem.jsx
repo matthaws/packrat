f99c5eed4fe1d9d24dff64acb286b0393bcc52ae
Ommi Shimizu <oshimizu15@gmail.com>
2018-02-15 19:40:02 -0500

    Add delimiter to categories

--- src/components/articles/articleIndexItem.jsx
+++ src/components/articles/articleIndexItem.jsx
@@ -4,7 +4,7 @@ import "./articleIndexItem.css";
 const ArticleIndexItem = ({article, categories}) => {
   return (
     <tr>
-      <td>{article.categoryIds.map((id) => categories[id].subject)}</td>
+      <td>{article.categoryIds.map((id) => categories[id].subject).join(" // ")}</td>
       <td><a href={article.url} target="_blank">{article.title}</a></td>
       <td>{article.description}</td>
       <td>{new Date(article.createdAt).toDateString()}</td>
